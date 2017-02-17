510cf2b44977582080b7e097725d1715e6152227
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-17 08:15:12 -0700

    complete zip format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,13 +5,13 @@ puts "EventManager Initialized"
 
 contents = CSV.open "./event_attendees.csv", headers: true, header_converters: :symbol
 
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+
 contents.each do |line|
-  zip = line[:zipcode].to_s
-  if zip.length <= 5
-    zip = zip.rjust(5, "0")
-  elsif zip.length > 5
-    zip = zip.slice(0..4)
-  end
+  zip = clean_zipcode(line[:zipcode])
   name = line[:first_name]
   puts "#{name} lives in the #{zip}"
 end
