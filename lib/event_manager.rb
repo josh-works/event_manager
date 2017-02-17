5ffb9511694270f21150596be7d7a59ddf7f28f9
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-17 08:15:54 -0700

    cleanup

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'csv'
 
 puts "EventManager Initialized"
@@ -9,21 +8,8 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
-
 contents.each do |line|
   zip = clean_zipcode(line[:zipcode])
   name = line[:first_name]
   puts "#{name} lives in the #{zip}"
 end
-
-
-# csv = File.open("./event_attendees.csv", "r").read.split("\n")
-#
-# puts csv
-
-
-# sample data
-# ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode
-# 1,11/12/08 10:47,Allison,Nguyen,arannon@jumpstartlab.com,6154385000,3155 19th St NW,Washington,DC,20010
-# 2,11/12/08 13:23,SArah,Hankins,pinalevitsky@jumpstartlab.com,414-520-5000,2022 15th Street NW,Washington,DC,20009
-# 3,11/12/08 13:30,Sarah,Xx,lqrm4462@jumpstartlab.com,(941)979-2000,4175 3rd Street North,Saint Petersburg,FL,33703
